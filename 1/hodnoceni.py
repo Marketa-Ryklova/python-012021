c192506ddfaa29363dc21ccffde96496d97f9c7d
Your Name <you@example.com>
2021-02-22 22:42:09 +0100

    Hodnocení úkolů

---
+++ 1/hodnoceni.py
@@ -0,0 +1,27 @@
+
+
+# 1. úkol : 1 bod
+# super
+# print(f"Balík byl předán kurýrovi")
+#       f  nemusí tam být f
+# print("Balík byl předán kurýrovi")
+
+
+# 2. úkol : 1 bod
+# super
+# drobnost, ale jedeme dál (přidej na řádek 24 print(sklad))
+
+# 3. úkol : 1 bod
+# super
+
+# 4. úkol : 0 bod
+# pláču není čtyřka
+
+# 5. úkol : 1 bod
+# super
+# zajímavé a pěkné řešení
+# kniha, která se prodá jenom v rove 2019, se nevytiskne, šlo by to upravit?
+
+
+
+# Skvělá práce, jen tak dál 👍
