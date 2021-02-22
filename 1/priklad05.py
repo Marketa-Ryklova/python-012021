20109b54daea671918fe37d99eb5de21dcee0355
Marketa-Ryklova <Marketa9292>
2021-02-22 13:03:12 +0100

    příklad05

---
+++ 1/priklad05.py
@@ -0,0 +1,22 @@
+prodeje2019 = {
+    "Zkus mě chytit": 4165,
+    "Vrah zavolá v deset": 5681,
+    "Zločinný steh": 2565,
+}
+
+prodeje2020 = {
+    "Zkus mě chytit": 3157,
+    "Vrah zavolá v deset": 3541,
+    "Vražda podle knihy": 2510,
+    "Past": 2364,
+    "Zločinný steh": 5412,
+    "Zkus mě chytit": 6671,
+}
+prodeje2020['Zkus mě chytit'] = 9828
+titul = input("Zadej název knihy: ")
+titul = str(titul)
+
+if titul in prodeje2019 and titul in prodeje2020:
+  print (prodeje2019[titul] + (prodeje2020[titul]))
+else:
+  print(prodeje2020[titul])
