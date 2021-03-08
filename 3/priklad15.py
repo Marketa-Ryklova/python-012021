8dbe8346861632934d79fe67784535c004f5f768
Marketa-Ryklova <Marketa9292>
2021-03-08 12:37:44 +0100

    Příklad 15

--- 3/priklad15.py
+++ 3/priklad15.py
@@ -1,7 +1,33 @@
 from datetime import datetime, timedelta
 performance_start = input("Zadejte datum představení: ")
-performance_start = datetime.fromisoformat("2020-09-01")
-performance_start = datetime.strptime("21.7.2020", "%d.%m.%Y")
+start = datetime.strptime(performance_start, "%d. %m. %Y")
+people = int(input("Zadej počet osob: "))
+
+prvni_datum = datetime(2021, 7, 1)
+druhe_datum = datetime(2021, 8, 10)
+treti_datum = datetime(2021, 8, 11)
+ctvrte_datum = datetime(2021, 8, 31)
+
+vstupenka1 = 250
+vstupenka2 = 180
+
+cena_vstupenky1 = people * vstupenka1
+cena_vstupenky2 = people * vstupenka2
+
+cena1 = (start <= druhe_datum)
+cena2 = (start >= treti_datum)
+
+
+if start < prvni_datum:
+  print("Letní kino je v tomto termínu uzavřené")
+elif cena1:
+  print(f"Cena vstupenky je {cena_vstupenky1}")
+elif start > ctvrte_datum:
+  print("Letní kino je v tomto termínu uzavřené")
+elif cena2:
+  print(f"Cena vstupenky je {cena_vstupenky2} ")
+
+
 
 
 
