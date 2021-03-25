89c457bcaf5bf7cf31fbf19db9eed02a18eca763
Marketa-Ryklova <Marketa9292>
2021-03-25 16:35:05 +0100

    priklad 25

--- 5/priklad25.py
+++ 5/priklad25.py
@@ -2,7 +2,17 @@ import pandas
 import wget
 #wget.download("https://raw.githubusercontent.com/pesikj/python-012021/master/zadani/5/temperature.csv")
 temperature = pandas.read_csv("temperature.csv")
-temperature = temperature.set_index("Day")
+temperature = temperature.set_index("City")
 
-#print(temperature.head(5))
-temperature["13. listopadu 2017"]
+print(temperature.head(5))
+teplota_13 = temperature[temperature["Day"] == 13]
+#print(teplota_13)
+#dotaz nařádky z 13.listopadu 2017
+
+teplota_v_us = temperature[(temperature["Day"] == 13) & (temperature["Country"] == "US")]
+print(teplota_v_us)
+#teploty 13.listopadu v US
+
+mesta = teplota_v_us.isin(["Washington", "Philadephia"])
+print(mesta)
+#Washington, Philadephia
