af4a9c4de8207a3eb01e3566dd74f8490c6539a4
Marketa-Ryklova <Marketa9292>
2021-04-23 12:00:04 +0200

    příklady 40-45

--- pujcovna/katalog/models.py
+++ pujcovna/katalog/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Auto(models.Model):
+  SPZ = models.CharField(max_length=10)
+  Typ = models.CharField(max_length=100)
+  KM = models.IntegerField()
+  STK = models.DateField()
+
+class Zakaznik(models.Model):
+  Jméno= models.CharField(max_length=30)
+  Příjmení = models.CharField(max_length=30)
+  ČísloŘP= models.CharField(max_length=15)
+  Datum_narození = models.DateField()
+
+class Vypujcka(models.Model):
+  Začátek = models.DateTimeField()
+  Konec = models.DateTimeField()
+  Cena = models.IntegerField()
+
